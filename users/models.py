5fab6c4ebf8f1325b51f8e64a03f29f42c9bc920
Vitoria <hvitoriaalmeida@gmail.com>
2025-03-27 23:05:17 +0000

    Making some changes and slowly implementing concepts to handle the DB

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#class User(models.User):
+    #userID = models.IntegerField
+    #userLastName = models.CharField(max_length=50)
+    #userFirstName = models.CharField(max_length=50)
+    #userPassword =
+    #userRole =
+    #userEmail = models.EmailField(max_length=254, **options)
+    #userRegID =
+    
