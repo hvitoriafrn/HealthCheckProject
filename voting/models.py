2b33960652701c62ccb7e6279e7e36dd286d4b36
Pawel Krezel <pawelkrezel@Mac.communityfibre.co.uk>
2025-04-21 01:50:00 +0100

    voting functionality has been added. users can now vote after logging in. front end is at its bare minimum and I will need to work on cosmetics

--- voting/models.py
+++ voting/models.py
@@ -38,11 +38,11 @@ class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # which user voted
     session = models.ForeignKey(Session, on_delete=models.CASCADE) # in which session was that
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # what was the question
-    choice = models.CharField(max_length=5) # did the user answer the question
+    choice = models.CharField(max_length=5) # how did the user answer the question
     comment = models.TextField(blank=True, null=True) # optional comment the user can add, field not required
 
     class Meta:
-        unique_together = ('user', 'session', 'question') # idk if that's still needed 
+        unique_together = ('user', 'session', 'question') # idk if that's still needed, I kept it to be safe 
 
     def __str__(self):
         return f"Vote ID:{self.pk} - {self.user.username} - {self.session.title} - {self.question.question_content}"
--- voting/views.py
+++ voting/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Session, Question, Vote
 
-# Create your views here.
+@login_required
+def dashboard(request):
+    #query to find sessions that have not been completed by the logged in user
+    pending_sessions = Session.objects.filter(users=request.user).exclude(submitted_by=request.user)
+    context = {
+        'user':request.user,
+        'pending_sessions':pending_sessions
+    }
+    return render(request, 'voting/dashboard.html', context)
 
-def vote_home(request):
-    return render(request, 'voting/home.html')
+@login_required
+def session(request, session_id):
+    session = get_object_or_404(Session, pk=session_id)
+    questions = session.questions_included.all()
 
-def submit_vote(request):
-    return render(request, 'voting/submit.html')
+    # troubleshooting - if user already submitted this session
+    if session.is_completed_by_user(request.user):
+        return render(request, 'voting/already_submitted.html', {'session': session})
 
-def view_results(request):
-    return render(request, 'voting/results.html')
+    # if the form was submitted
+    if request.method == 'POST':
+        for question in questions:
+            choice = request.POST.get(f"{question.pk}-colour")
+            comment = request.POST.get(f"{question.pk}-comment", '').strip()
+
+            if choice in Vote.vote_options:
+                Vote.objects.update_or_create(
+                    user=request.user,
+                    session=session,
+                    question=question,
+                    defaults={
+                        'choice': choice,
+                        'comment': comment
+                    }
+                )
+
+        # Mark session as submitted and go back to the dashborad 
+        session.submitted_by.add(request.user)
+        return redirect('/voting/') 
+    
+    # if the form was not submitted and we need to render questions 
+    context = {
+        'session': session,
+        'questions': questions
+    }
+    return render(request, 'voting/session.html', context)
