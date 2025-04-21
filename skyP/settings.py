3c1512e30bf2d1fa3d3e26144ada4dd37396c8b7
Pawel Krezel <pawelkrezel@Mac.communityfibre.co.uk>
2025-04-21 01:50:00 +0100

    voting functionality has been added. users can now vote after logging in. front end is at its bare minimum and I will need to work on cosmetics

--- skyP/settings.py
+++ skyP/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'skyP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), '/voting/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- voting/urls.py
+++ voting/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'voting'
 urlpatterns = [
-    path('', views.vote_home, name='vote_home'),  # The main dashboard the user sees after logging in
-   
+    path('', views.dashboard, name='dashboard'),
+    path('<int:session_id>', views.session, name='session'),
+
 ]
