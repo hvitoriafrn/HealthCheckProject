2065bf4104b143cbc24db126134a260f2cb15648
yxasmin <w1985103@westminster.ac.uk>
2025-04-25 19:35:32 +0100

    Completed navbar, added a border and also made the nav options bigger. Added a sign out button and verified that the navbar appears differently for different users.

--- voting/urls.py
+++ voting/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('submit/', views.submit_vote, name='submit_vote'),  # Vote submission page
     path('results/', views.view_results, name='view_results'),  # View results page  
      path('choose-session/', views.choose_session, name='choose_session'),  # Voting sessions
-    path('vote-summary/', views.vote_summary, name='vote_summary'),  # Team summary  
+    path('vote-summary/', views.vote_summary, name='vote_summary'),  # View summary  
     path('dashboard/', views.dashboard_view, name='dashboard'),  # Dashboard view
 ]
