2737f9ce30714abd50a05996ec275b3f2c0fdd17
Pawel Krezel <122262516+w1837610@users.noreply.github.com>
2025-04-30 16:55:47 +0100

    Update admin.py
    Department added to the admin dashboard

--- voting/admin.py
+++ voting/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Session, Question, Vote
+from .models import Session, Question, Vote, Department
 admin.site.register(Session)
 admin.site.register(Question)
 admin.site.register(Vote)
+admin.site.register(Department)
