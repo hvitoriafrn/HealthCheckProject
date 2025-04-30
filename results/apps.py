3bd8c4b2f20c7123728f1762537b86bc4ea42977
Vitoria <hvitoriaalmeida@gmail.com>
2025-04-30 10:54:19 +0100

    deleted unused results app

--- results/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResultspageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'results'
--- results/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.results_home, name='results_home'),  # Results homepage
-#     path('summary/', views.summary, name='summary'),  # Summary of votes
-]
